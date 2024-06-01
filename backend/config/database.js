713ae112b8760d5247c630625b90813dabecf5dd
Harshvardhan <shreeharshpb11@gmail.com>
2024-06-01 13:08:07 +0530

    add database connection

---
+++ backend/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+exports.connect = () => {
+    mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => {
+        console.log('Mongo Db Connected')
+    }).catch((error) => {
+        console.log('Mongo Connection Failed')
+    })
+}
