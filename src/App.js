96a2eb0a5ba81970a7b6abc0b8b2788b15de78c7
Harshvardhan <shreeharshpb11@gmail.com>
2024-06-14 00:56:50 +0530

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import "./App.css";
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
 
-function App() {
+const App = () => {
   return (
-    <main class="flex justify-center gap-4 flex-col min-h-screen">
-      <h1 class="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
-      <p class="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
-      <img src="https://bit.ly/3wsmzTy" alt="meme" class="mx-auto" />
-    </main>
-  );
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </div>
+  )
 }
 
-export default App;
+export default App
