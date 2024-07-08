6100da7478442b6629ed9cfd57f6dd89317a9a11
Harshvardhan <shreeharshpb11@gmail.com>
2024-07-09 02:37:08 +0530

    error page

---
+++ src/pages/Error.jsx
@@ -0,0 +1,9 @@
+const Error = () => {
+  return (
+    <div className="flex justify-center items-center text-6xl text-white h-[90vh]">
+      Error - 404 Not Found
+    </div>
+  );
+};
+
+export default Error;
