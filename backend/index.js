0e1ad3216a6f83b63d691232a50696cebfb2d6b8
Harshvardhan <shreeharshpb11@gmail.com>
2024-07-09 14:14:52 +0530

    fix bugs

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"studynotion-educationtech-hub.vercel.app",
 		credentials:true,
 	})
 )
